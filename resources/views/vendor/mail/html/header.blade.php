7b374ff2056b07caf179a4d6f974e8afe3783a8c
Donny Hidayat <donny@MacBook-Air-Donny.local>
2026-06-12 13:31:55 +0700

    Ganti logo Laravel di email dengan brand donnyhidayat.blog

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<span style="font-size:20px;font-weight:800;color:#1d4ed8;letter-spacing:-0.5px;">donnyhidayat<span style="color:#6b7280;">.blog</span></span>
+</a>
+</td>
+</tr>
